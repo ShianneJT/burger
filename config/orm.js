a647d8c0345a54076a75d7028f9e85fef5cb630e
Shianne <ShianneJT@gmail.com>
2020-05-31 20:42:10 -0600

    adds server and orm

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,50 @@
 var connection = require('../config/connection.js');
 
 var orm = {
-    selectAll: function(tableInput, cb) {
-        var queryString = 'SELECT * FROM ??';
-        connection.query(queryString, [tableInput, colInput], function(err,res) {
-            if (err) throw err;
+    selectAll: function(table, cb) {
+        var queryString = 'SELECT * FROM ' + table;
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
         });
-        console.log(cb(res));
-    }
-}
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = 'INSERT INTO' + table;
 
-// insertOne()
-// updateOne()
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, res) {
+          if (err) {
+            throw err;
+          }
+          cb(res);
+    });
+},
+    updateOne: function(table, vals, condition, cb) {
+        var queryString = 'UPDATE ' + table;
+
+        queryString += ' SET ';
+        queryString += vals;
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+};
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -8,11 +8,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// routes
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+// listener
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
