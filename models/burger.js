a949c746e167e65e5bf09e27e402a56f35eab876
Shianne <ShianneJT@gmail.com>
2020-06-07 14:51:18 -0600

    updates burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,23 @@
 var orm = require('../config/orm.js');
 
-// create the code that will call the ORM functions using burger specific input for the ORM.
-// selectAll()
-// insertOne()
-// updateOne()
-
 var burger = {
     selectAll: function(cb) {
         orm.selectAll('burgers', function(res) {
             cb(res);
         });
     },
+
     insertOne: function(cols, vals, cb) {
         orm.insertOne('burgers', cols, vals, function(res) {
             cb(res);
         });
     },
+
     updateOne: function(objColVals, condition, cb) {
         orm.updateOne('burgers', objColVals, condition, function (res) {
             cb(res);
         });
     }
-}
+};
 
 module.exports = burger;
