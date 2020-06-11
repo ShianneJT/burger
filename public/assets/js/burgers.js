da04240ca83e710b5164640cb8e038d25f52c362
Shianne <ShianneJT@gmail.com>
2020-06-10 20:41:05 -0600

    Finished functionality and styling

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devourStatus").on("click", function(event) {
+      var id = $(this).data("id");
+      var eaten = $(this).data("eaten");
+  
+      var devourState = {
+        devoured: eaten
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devourState
+      }).then(
+        function() {
+          console.log("changed to", eaten);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#bn").val().trim(),
+        devoured: 0
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
